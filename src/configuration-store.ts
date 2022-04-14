05dae2641c7a04cc4d56266694fe818a841dd011
Peter Loomis <loomispeter@gmail.com>
2022-04-14 15:34:30 -0700

    implement in-memory cache

--- src/configuration-store.ts
+++ src/configuration-store.ts
@@ -1,3 +1,28 @@
+import * as NodeCache from 'node-cache';
+
 export interface IConfigurationStore<T> {
   getConfiguration(key: string): Promise<T>;
+  setConfigurations(configs: Record<string, T>): Promise<void>;
+}
+
+/**
+ * Default ConfigurationStore implementation. Sets and retrieves entries from an in-memory cache.
+ */
+export class InMemoryConfigurationStore<T> implements IConfigurationStore<T> {
+  private cache: NodeCache;
+  constructor(private ttlSeconds: number) {
+    this.cache = new NodeCache({ stdTTL: ttlSeconds });
+  }
+
+  async getConfiguration(key: string): Promise<T> {
+    return this.cache.get<T>(key);
+  }
+
+  async setConfigurations(configs: Record<string, T>) {
+    const cacheEntries = Object.entries(configs).map(([key, val]) => ({
+      val,
+      key,
+    }));
+    this.cache.mset(cacheEntries);
+  }
 }
