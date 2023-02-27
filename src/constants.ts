4880c9670312c64a68dbd530ab28bd34aac3377d
Evan Miller <evan@geteppo.com>
2023-02-27 17:04:04 -0500

    Increase request timeout to 5s

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,6 @@ const MINUTE_MILLIS = 60 * SECOND_MILLIS;
 export const POLL_INTERVAL_MILLIS = 5 * MINUTE_MILLIS;
 export const JITTER_MILLIS = 30 * SECOND_MILLIS;
 export const MAX_CACHE_ENTRIES = 1000;
-export const REQUEST_TIMEOUT_MILLIS = SECOND_MILLIS;
+export const REQUEST_TIMEOUT_MILLIS = 5 * SECOND_MILLIS;
 
 export const BASE_URL = 'https://eppo.cloud/api';
