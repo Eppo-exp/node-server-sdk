e28cee0ba8df2dbe0cbb20d2ed6622946a86db7b
Pavel Khrebto <83206611+pavelflux@users.noreply.github.com>
2023-03-01 14:06:50 +0200

    Pavel/fixes to download files (#25)
    * 1.2.0
    
    * Fix for incorrect download of test cases if data folder does not exist (previously if was only correct in makefile); upgrade google-storage lib

--- test/globalSetup.ts
+++ test/globalSetup.ts
@@ -2,24 +2,39 @@ import * as fs from 'fs';
 
 import { Storage } from '@google-cloud/storage';
 
-import { TEST_DATA_DIR } from './testHelpers';
+import { TEST_DATA_DIR, ASSIGNMENT_TEST_DATA_DIR } from './testHelpers';
 
 const storage = new Storage();
+const bucket = storage.bucket('sdk-test-data');
 
 async function downloadTestDataFiles() {
-  const [files] = await storage.bucket('sdk-test-data').getFiles({
-    prefix: 'assignment/test-case',
-  });
-  return Promise.all(
-    files.map((file, index) => {
-      return file.download({ destination: `${TEST_DATA_DIR}test-case-${index}.json` });
+  const [files] = await bucket.getFiles();
+
+  await downloadFiles(files);
+}
+
+const downloadFile = async (file) => {
+  const destination = `${TEST_DATA_DIR}${file.name}`;
+  await file.download({ destination });
+};
+
+const downloadFiles = async (files) => {
+  await Promise.all(
+    files.map(async (file) => {
+      if (file.name.endsWith('/')) {
+        const [subfiles] = await bucket.getFiles({ prefix: file.name });
+        await downloadFiles(subfiles);
+      } else {
+        await downloadFile(file);
+      }
     }),
   );
-}
+};
 
 export default async () => {
   if (!fs.existsSync(TEST_DATA_DIR)) {
     fs.mkdirSync(TEST_DATA_DIR);
+    fs.mkdirSync(ASSIGNMENT_TEST_DATA_DIR);
     await downloadTestDataFiles();
   }
 };
