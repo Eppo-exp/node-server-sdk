503285fbb665119710ab527c0a46d9e6b9c7b1f4
Eric Petzel <epetzel@gmail.com>
2023-04-21 10:15:40 +0900

    Reduce update latency by using new config endpoint (#28)
    * reduce update latency by using new config endpoint
    
    * increase version to 1.3.0

--- src/constants.ts
+++ src/constants.ts
@@ -6,4 +6,4 @@ export const JITTER_MILLIS = 30 * SECOND_MILLIS;
 export const MAX_CACHE_ENTRIES = 1000;
 export const REQUEST_TIMEOUT_MILLIS = 5 * SECOND_MILLIS;
 
-export const BASE_URL = 'https://eppo.cloud/api';
+export const BASE_URL = 'https://fscdn.eppo.cloud/api';
