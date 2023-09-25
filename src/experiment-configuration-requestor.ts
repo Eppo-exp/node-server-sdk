aaaa76eda6c2d71c467993fb5571f64e3283f264
Sameeran Kunche <skunche@gmail.com>
2023-09-25 16:39:57 -0700

    Clean up EppoNodeExperimentConfigurationRequestor

--- src/experiment-configuration-requestor.ts
+++ src/experiment-configuration-requestor.ts
@@ -4,15 +4,8 @@ import { IExperimentConfiguration } from '@eppo/js-client-sdk-common/dist/dto/ex
 import { IEppoConfigurationStore, commonCompatibleConfigurationStore } from './configuration-store';
 import HttpClient from './http-client';
 
-interface IRandomizedAssignmentConfig {
-  flags: Record<string, IExperimentConfiguration>;
-}
-
-const RAC_ENDPOINT = '/randomized_assignment/v3/config';
-
 export default class EppoNodeExperimentConfigurationRequestor extends ExperimentConfigurationRequestor {
   public configStore: IConfigurationStore;
-  private _httpClient: HttpClient;
 
   constructor(
     configurationStore: IEppoConfigurationStore<IExperimentConfiguration>,
@@ -21,13 +14,5 @@ export default class EppoNodeExperimentConfigurationRequestor extends Experiment
     const configStore = new commonCompatibleConfigurationStore(configurationStore);
     super(configStore, httpClient);
     this.configStore = configStore;
-    this._httpClient = httpClient;
-  }
-
-  async fetchAndStoreConfigurations(): Promise<Record<string, IExperimentConfiguration>> {
-    const responseData = await this._httpClient.get<IRandomizedAssignmentConfig>(RAC_ENDPOINT);
-
-    this.configStore.setEntries(responseData?.flags ?? {});
-    return responseData?.flags ?? {};
   }
 }
