6a9046cf79e67018f0f4b75dbb37e39b31dd5228
Leo Romanovsky <leoromanovsky@gmail.com>
2023-10-23 06:07:33 -0700

    remove untestable method

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,12 +1,13 @@
 import * as td from 'testdouble';
 
 import apiServer from '../test/mockApiServer';
+import { IAssignmentTestCase, readAssignmentTestData, ValueTestType } from '../test/testHelpers';
 
 import EppoClient from './client/eppo-client';
 import ExperimentConfigurationRequestor from './experiment-configuration-requestor';
 import { IPoller } from './poller';
 
-import { IAssignmentEvent, IAssignmentLogger, init } from '.';
+import { getInstance, IAssignmentEvent, IAssignmentLogger, init } from '.';
 
 describe('EppoClient E2E test', () => {
   const mockLogger: IAssignmentLogger = {
@@ -88,6 +89,52 @@ describe('EppoClient E2E test', () => {
     });
   });
 
+  describe('getAssignment', () => {
+    it.each(readAssignmentTestData())(
+      'test variation assignment splits',
+      async ({
+        experiment,
+        valueType = ValueTestType.StringType,
+        subjects,
+        subjectsWithAttributes,
+        expectedAssignments,
+      }: IAssignmentTestCase) => {
+        console.log(`---- Test Case for ${experiment} Experiment ----`);
+
+        const assignments = getAssignmentsWithSubjectAttributes(
+          subjectsWithAttributes
+            ? subjectsWithAttributes
+            : subjects.map((subject) => ({ subjectKey: subject })),
+          experiment,
+          valueType,
+        );
+
+        switch (valueType) {
+          case ValueTestType.BoolType: {
+            const boolAssignments = assignments.map((a) => a ?? null);
+            expect(boolAssignments).toEqual(expectedAssignments);
+            break;
+          }
+          case ValueTestType.NumericType: {
+            const numericAssignments = assignments.map((a) => a ?? null);
+            expect(numericAssignments).toEqual(expectedAssignments);
+            break;
+          }
+          case ValueTestType.StringType: {
+            const stringAssignments = assignments.map((a) => a ?? null);
+            expect(stringAssignments).toEqual(expectedAssignments);
+            break;
+          }
+          case ValueTestType.JSONType: {
+            const jsonStringAssignments = assignments.map((a) => a ?? null);
+            expect(jsonStringAssignments).toEqual(expectedAssignments);
+            break;
+          }
+        }
+      },
+    );
+  });
+
   it('assigns subject from overrides when experiment is enabled', () => {
     const mockConfigRequestor = td.object<ExperimentConfigurationRequestor>();
     const mockPoller = td.object<IPoller>();
@@ -164,4 +211,61 @@ describe('EppoClient E2E test', () => {
     const assignment = client.getAssignment('subject-10', flagKey, subjectAttributes);
     expect(assignment).toEqual('control');
   });
+
+  function getAssignmentsWithSubjectAttributes(
+    subjectsWithAttributes: {
+      subjectKey: string;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      subjectAttributes?: Record<string, any>;
+    }[],
+    experiment: string,
+    valueTestType: ValueTestType = ValueTestType.StringType,
+  ): (unknown | null)[] {
+    const client = getInstance();
+    return subjectsWithAttributes.map((subject) => {
+      switch (valueTestType) {
+        case ValueTestType.BoolType: {
+          const ba = client.getBoolAssignment(
+            subject.subjectKey,
+            experiment,
+            subject.subjectAttributes,
+          );
+          if (ba === null) return null;
+          return ba as boolean;
+        }
+        case ValueTestType.NumericType: {
+          const na = client.getNumericAssignment(
+            subject.subjectKey,
+            experiment,
+            subject.subjectAttributes,
+          );
+          if (na === null) return null;
+          return na as number;
+        }
+        case ValueTestType.StringType: {
+          const sa = client.getStringAssignment(
+            subject.subjectKey,
+            experiment,
+            subject.subjectAttributes,
+          );
+          if (sa === null) return null;
+          return sa as string;
+        }
+        case ValueTestType.JSONType: {
+          const sa = client.getJSONStringAssignment(
+            subject.subjectKey,
+            experiment,
+            subject.subjectAttributes,
+          );
+          const oa = client.getParsedJSONAssignment(
+            subject.subjectKey,
+            experiment,
+            subject.subjectAttributes,
+          );
+          if (oa == null || sa === null) return null;
+          return sa as string;
+        }
+      }
+    });
+  }
 });
